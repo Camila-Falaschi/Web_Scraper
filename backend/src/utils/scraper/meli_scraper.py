e06f9c4ecb02f3ec1823861f10454cac23d54b95
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-04-15 03:30:16 +0200

    :white_check_mark: Chore: Add 'scrape_categories_list' function to find the url of the chosen category page

--- backend/src/utils/scraper/meli_scraper.py
+++ backend/src/utils/scraper/meli_scraper.py
@@ -3,26 +3,59 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-BASE_URL = "https://lista.mercadolivre.com.br"
-DRIVER_PATH = '/path/to/chromedriver'
+class Meli_Scraper:
+    """
+    Creates a new Scraper instance to get data from the Mercado Livre website.
 
-def scrape_navbar_category():
-    """This function returns the URL to the page with the categories list."""
-    
-    browser_options = ChromeOptions()
-    driver = Chrome(ChromeDriverManager().install(), options=browser_options)
-    driver.get(BASE_URL)
-    
-    categories = driver.find_element(By.CLASS_NAME, "nav-menu-categories-link")
-    return categories.get_attribute("href")
-    
-def get_mobiles():
-    pass
-    
-def get_refrigerator():
-    pass
-    
-    
-def get_tv():
-    pass
-    
+    Attributes
+    ----------
+    browser_options: <class 'selenium.webdriver.chrome.options.Options'>
+        Creates a new instance of ChromeOptions.
+    browser_options.headless: bool
+        Defines browser options headless to True.
+    driver : <class 'selenium.webdriver.chrome.webdriver.WebDriver'>
+        Creates a new instance of the Chrome driver.
+
+    Methods
+    -------
+    scrape_homepage_navbar() -> str
+        Returns the URL to the page with the categories list.
+    scrape_categories_list(category:str) -> str
+        Returns the URL to the page with the categories list.
+    """
+
+    def __init__(self):
+        self.browser_options = ChromeOptions()
+        self.browser_options.headless = True
+        self.driver = Chrome(
+            ChromeDriverManager().install(), options=self.browser_options
+        )
+        self.BASE_URL = "https://lista.mercadolivre.com.br"
+
+    def scrape_homepage_navbar(self) -> str:
+        """This function returns the URL to the page with the categories list."""
+        
+        self.driver.get(self.BASE_URL)
+        categories = self.driver.find_element(By.CLASS_NAME, "nav-menu-categories-link")
+        return categories.get_attribute("href")
+
+
+    def scrape_categories_list(self, category:str) -> str:
+        """This function returns the URL of the chosen category page."""
+        
+        category_list_url = self.scrape_homepage_navbar()
+        self.driver.get(category_list_url)
+        page_url = self.driver.find_element(
+            By.XPATH, f"//h3[contains( text( ), '{category}')]"
+        ).find_element(By.XPATH, "..").get_attribute("href")
+        return page_url
+
+    def get_mobiles(self):
+        self.scrape_categories_list("Celulares e Smartphones")
+        print()
+
+    def get_refrigerator(self):
+        pass
+
+    def get_tv(self):
+        pass
